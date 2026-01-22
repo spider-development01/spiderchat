00fca43900d7eda86d47e0721dc97d03583057b7
Spider-Development <147894555+spider-development01@users.noreply.github.com>
2026-01-22 07:56:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const SUPABASE_URL = 'https://ueswgpkqqymgylhdjhek.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlc3dncGtxcXltZ3lsaGRqaGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMzg3MDAsImV4cCI6MjA4NDYxNDcwMH0.kBDpfxQ3_pDE_CxdXlXVJQdGrR-5EQlc8yq9xO5932E';
+
+export { SUPABASE_URL, SUPABASE_ANON_KEY };
